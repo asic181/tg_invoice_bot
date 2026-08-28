4657698e4e2d174104bf57229825aab577d6d73f
Dmitry <asic181@yandex.ru>
2026-08-28 21:32:59 +0400

    First commit invoice bot

--- bitrix_service.py
+++ bitrix_service.py
@@ -7,11 +7,7 @@ load_dotenv()
 
 WEBHOOK_URL = (os.getenv("BITRIX24_WEBHOOK_URL") or "").rstrip("/")
 ENTITY_TYPE_ID = int(os.getenv("BITRIX24_ENTITY_TYPE_ID", 31))
-
-# Коды кастомных полей из .env
-UF_CHAT_ID = os.getenv("UF_TG_CHAT_ID", "ufCrm_TG_CHAT_ID")
-UF_MSG_ID = os.getenv("UF_TG_MSG_ID", "ufCrm_TG_MSG_ID")
-UF_FILE = os.getenv("UF_INVOICE_FILE", "ufCrm_INVOICE_FILE")
+UF_INVOICE_FILE = os.getenv("UF_INVOICE_FILE", "ufCrm_SMART_INVOICE_1787937924417")
 
 async def create_invoice_in_bitrix(
     file_bytes: bytes,
@@ -28,22 +24,15 @@ async def create_invoice_in_bitrix(
     file_b64 = base64.b64encode(file_bytes).decode("utf-8")
     
     title = f"Счет от {user_name} ({file_name})"
-    full_comment = f"Отправитель: {user_name} (@{tg_username})<br>Чат ID: {chat_id}<br>Сообщение ID: {message_id}<br>Комментарий: {comment}"
-    
-    fields = {
-        "title": title,
-        "comments": full_comment,
-        UF_CHAT_ID: str(chat_id),
-        UF_MSG_ID: str(message_id)
-    }
-
-    # Если в CRM настроено поле типа «Файл»
-    if UF_FILE:
-        fields[UF_FILE] = [file_name, file_b64]
+    full_comment = f"Отправитель: {user_name} (@{tg_username})<br>Чат: {chat_id}<br>Сообщение: {message_id}<br>Комментарий: {comment}"
     
     payload = {
         "entityTypeId": ENTITY_TYPE_ID,
-        "fields": fields
+        "fields": {
+            "title": title,
+            "comments": full_comment,
+            UF_INVOICE_FILE: [file_name, file_b64]
+        }
     }
     
     async with aiohttp.ClientSession() as session:
